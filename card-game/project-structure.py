021f572c7cd2d942c92be7b80b08752562df2e67
KX <KX@KX-3.local>
2018-11-21 23:20:27 +0800

    correct description

--- card-game/project-structure.py
+++ card-game/project-structure.py
@@ -1,44 +1,73 @@
 #Stage 1
 """Turtle draw boss, boss health
    Turtle draw player, player attacks, player health"""
+
+"""
+INPUT: none
+RETURN: tuple (string castSpell, boolean fail)
+"""
 def getPlayerInput(): #use normal input()
     try:
         eval(playerMove)
     except:
         print("code runtime error")
+        castSpell = ""
     finally:
         if eval(userInput) == attack1:
             castSpell = attack1
             fail = False
-            castSpell()
-            return(castSpell, playerFail) 
+            return (castSpell, playerFail) 
             """could add another element to store error message, maybe show 
             the message in turtle for few secs"""
             """more elifs for other spell name"""
         else:
             fail = True
-            castSpell = ""
-            return [castSpell, fail]
+            return (castSpell, fail)
+ #??is castSpell a string, if it is, when will we convert it into integer since 
+ #it needs to be the degree of damage rite????
 
+"""
+calls playerHitBoss or bossHitPlayer
+INPUT: (string castSpell, boolean fail)
+RETURN: health
+"""
 def castSpell(playerMove):
     if playerMove[1] == True:
-        #Boss hit player() Boss hits player, potentially increment counter to track number of fails
-        return
+        bossHitPlayer(attack, bossHealth)
+        #potentially increment counter to track number of fails
+        return bossHealth
     else:
-        return
+        playerHitBoss(playerMove[0], playerHealth)
         #player hit boss(playerMove[0]) Hit boss with playerMove[0] spell name. Spells defined here
+        return playerHealth
 
-def playerHitBoss(attackBoss):
+"""
+decrease boss health by (attack)
+INPUT: attack, bossHealth
+RETURN: bossHealth
+"""
+def playerHitBoss(attack, bossHealth):
     return
 """attack1 - deal x damage -> turtle draw attack1
 attack2 - deal x+y damage -> turtle draw attack2
 ..."""
 
-def bossHitPlayer(attack):
+"""
+decrease player health by (attack)
+INPUT: attack, playerHealth
+RETURN: playerHealth
+"""
+def bossHitPlayer(attack, playerHealth):
     #attack - deal z damage to player -> draw boss attack1
     return
 
-def checkHealth():
+
+"""
+if health <0 return false else return true
+INPUT: playerHealth, bossHealth
+RETURN: boolean
+"""
+def checkHealth(playerHealth, bossHealth):
     return
     """if bossHealth <= 60%: this is optional, without it then we only need to make attack spells
         we can add it after the core stuff is done
@@ -57,17 +86,13 @@ def checkHealth():
         exit this stage()
         break"""
     
-
 def stage():
     playerHealth = 100
     bossHealth = 100
     flag = True
+
     while flag:
         playerMove = getPlayerInput()
-
-
-        playerHitBoss(attack)
-        bossHitPlayer(attackBoss)
-
+        castSpell(playerMove)
         flag = checkHealth()
 
