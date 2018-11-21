9d5bb7ffd5e855aab6d21ae69e5deabb6c3d728f
KX <KX@KX-3.local>
2018-11-21 22:34:29 +0800

    project structure

--- card-game/project-structure.py
+++ card-game/project-structure.py
@@ -2,7 +2,23 @@
 """Turtle draw boss, boss health
    Turtle draw player, player attacks, player health"""
 def getPlayerInput(): #use normal input()
-    return
+    try:
+        eval(playerMove)
+    except:
+        print("code runtime error")
+    finally:
+        if eval(userInput) == attack1:
+            castSpell = attack1
+            fail = False
+            castSpell()
+            return(castSpell, playerFail) 
+            """could add another element to store error message, maybe show 
+            the message in turtle for few secs"""
+            """more elifs for other spell name"""
+        else:
+            fail = True
+            castSpell = ""
+            return [castSpell, fail]
 
 def castSpell(playerMove):
     if playerMove[1] == True:
@@ -46,44 +62,12 @@ def stage():
     playerHealth = 100
     bossHealth = 100
     flag = True
-    while True:
+    while flag:
         playerMove = getPlayerInput()
-        try:
-            eval(playerMove)
-        except:
-            print("code runtime error")
-        finally:
-            if eval(userInput) == attack1:
-                castSpell = attack1
-                fail = False
-                castSpell()
-                return(castSpell, playerFail) #return to whr?
-                """could add another element to store error message, maybe show 
-                the message in turtle for few secs"""
-                """more elifs for other spell name"""
-            else:
-                fail = True
-                castSpell = ""
 
-                #attackBoss = certain number? *see suggestion below
-
-                return(castSpell, playerFail) #return to whr?
 
         playerHitBoss(attack)
         bossHitPlayer(attackBoss)
 
-        flag = checkHealth() #can return false if health<0 to exit loop?
+        flag = checkHealth()
 
-        """
-        should we use different variable for attackPlayer and attackBoss? 
-
-        let initial attackPlayer = 0, if input matches the spell, 
-        only we set attack using castSpell, and set attackPlayer back to 0
-        after we decrease boss health
-
-        let initial attackBoss = 0, if input doesnt match the spell,
-        set attackBoss to a certain number(will the damage to user will be the same
-        for every round?), and set attackBoss back to 0 at the end of bossHitPlayer
-
-        so we can still call both playerHitBoss and bossHitPlayer every round
-        """
