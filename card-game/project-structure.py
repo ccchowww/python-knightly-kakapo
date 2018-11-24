d984f77b8306e0a9c2a4823b56501b4f96d1752b
ccchowww <ccchowww@gmail.com>
2018-11-24 14:53:58 +0800

    function or object oriented

--- card-game/project-structure.py
+++ card-game/project-structure.py
@@ -4,17 +4,41 @@
 
 class Game(): #or should we name the class as Player(), den the two objects are boss and USER,
 #then we might replace every "player" we typed wv "user", or do u hv a better name
+'''
+we can call it Mom since it gives birth to boss and USER
+'''
 	health = 100
 	fail = False
 	attack = 0
 	castSpell = 0
 	"""i am confused with attack and castSpell, are both of them actually integer?
 	attack is the target and castSpell is the damage deal right?"""
+    '''
+    that was my bad, attack is name of spell to use and its only needed for playerHitBoss(), but i added
+    it for bossHitplayer().
+
+    so we dont need to pass in any health for both playerhitBoss and bossHitPlayer,
+    only playerHitBoss() needs attack/spellName, then inside it we check for that spell and deal
+    damage.
+
+    i also realize that i wanted to change boss/player health from inside another function, which
+    wont work without declaring it as nonlocal or global depending where we put the healths.
+
+    Right now we need to choose between function oriented or switch to object.
+        if we define playerHealth, bossHealth, other stats... in global scope it looks simpler to me, and
+        may be better when everyone is working on it together, then we change it using global declaration.
+
+        but i also like objects, because web is all objects.
+
+    '''
 
 	#move all the functions inside, except stage()?
 	#we may not need playerMove?
 	#and we may only need one function for hit(attack, (user or boss))?
 	#like this-?
+    '''
+    this should work if we do object oriented
+    '''
 	def castSpell(player):
 		if player.fail == True:
 			return hit(player)
@@ -29,56 +53,62 @@ class Game(): #or should we name the class as Player(), den the two objects are 
 
 """
 INPUT: none
-RETURN: tuple (string castSpell, boolean fail)
+RETURN: tuple (string spellToCast, boolean fail)
+                ^Name of spell to cast
 """
 def getPlayerInput(): 
     try:
         eval(playerMove)
     except:
         print("code runtime error")
-        castSpell = ""
+        spellToCast = ""
     finally:
         if eval(userInput) == attack1:
-            castSpell = attack1
+            spellToCast = attack1
             fail = False
-            return (castSpell, playerFail) 
+            return (spellToCast, playerFail) 
             """could add another element to store error message, maybe show 
             the message in turtle for few secs"""
             """more elifs for other spell name"""
         else:
             fail = True
-            return (castSpell, fail)
+            return (spellToCast, fail)
 
 """
-helper function, calls playerHitBoss or bossHitPlayer
-INPUT: playerMove
+helper function, calls playerHitBoss(spellToCast) or bossHitPlayer()
+INPUT: playerMove == [spellToCast, fail]
 RETURN: none
 """
 def castSpell(playerMove):
     if playerMove[1] == True:
-        return bossHitPlayer(attack, playerHealth)
+        # return bossHitPlayer(attack, playerHealth)
+        return bossHitPlayer(playerHealth)
         #potentially increment counter to track number of fails
     else:
         return playerHitBoss(playerMove[0], bossHealth)
         #player hit boss(playerMove[0]) Hit boss with playerMove[0] spell name. Spells defined here
 
 """
-decrease boss health by (attack)
-INPUT: attack, bossHealth
+this function is where we define all the spells for function oriented
+check for spellName     -- find matching spellName, decrease health, call SpellXAnimation()
+    global PlayerHealth       -- declare health as global, so it is referring to health in global scope
+    PlayerHealth -= attackDamage
+    SpellXAnimation()
+
+# INPUT: attack, bossHealth
+INPUT: spellName # only need spellName
 RETURN: none
 """
-def playerHitBoss(attack, bossHealth):
+def playerHitBoss(spellName):
     return
-"""attack1 - deal x damage -> turtle draw attack1
-attack2 - deal x+y damage -> turtle draw attack2
-..."""
 
 """
-decrease player health by (attack)
-INPUT: attack, playerHealth
+decrease player health by whatever we want, call bossHitanimation()
+# INPUT: attack, playerHealth
+INPUT: jk dont need input
 RETURN: none
 """
-def bossHitPlayer(attack, playerHealth):
+def bossHitPlayer():
     #attack - deal z damage to player -> draw boss attack1
     return
 
@@ -117,13 +147,12 @@ def stage():
         castSpell(playerMove)
         flag = checkHealth()
 
-"""
-Nope this is too hard.
-instead of separate functions calling draw animations,
-we could use a dictionary to store those values, in this case it would be playerHealth = 100
-bossHealth = 100, then we could add hitplayer = x, hitboss = y, spelltodraw = playerattack1
-Then we pass this dict into a drawing function (dict), that will for example check
-difference in health and draw the change, draw the spell, draw death animation, etc
-Essentially have all drawing related functions in one function.
-okies
-"""
+
+# Nope this is too hard. {
+# instead of separate functions calling draw animations,
+# we could use a dictionary to store those values, in this case it would be playerHealth = 100
+# bossHealth = 100, then we could add hitplayer = x, hitboss = y, spelltodraw = playerattack1
+# Then we pass this dict into a drawing function (dict), that will for example check
+# difference in health and draw the change, draw the spell, draw death animation, etc
+# Essentially have all drawing related functions in one function.
+# okies }
--- card-game/test.py
+++ card-game/test.py
@@ -1,2 +1,32 @@
-code = 'attackvar = "a" +"b" + "c"'
-exec(code)
+# Global scope
+xd = 1
+
+def funcScope():
+    # global xd     # global to refer to variables in global scope
+    xd = 5
+    xxx = 20
+    xxy = 100
+    def nestedFunctionScope():
+        nonlocal xd     # nonlocal to refer to variables from scope directly above current scope
+        nonlocal xxy # chain nonlocal declaration
+        xd = 10
+        xxx = 30    # reassign without nonlocal declaration, no change above
+        def superNestedFunctionScope():
+            nonlocal xxx    # this changes xxx in nestedFunctionScope, but not in funcScope.
+            nonlocal xxy    # can chain nonlocal declarations to go up further than one scope.
+            global xd
+            xxx = 50
+            xxy = 200
+            xd = 420 # can change global from any deeper place
+
+        superNestedFunctionScope()
+        print(xxx, "xxx nestedFuncScope")
+        print(xd, "xd nestedFuncScope")
+
+    nestedFunctionScope()
+    print(xxx, "xxx funcScope")
+    print(xd, "xd funcScope")
+    print(xxy, "xxy funcScope")
+
+funcScope()
+print(xd, "xd global")
